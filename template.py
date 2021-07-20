4becb95763796f5f213dd502ff23dd3fe5eaf877
Lars Keggenhoff <lars@kghf.io>
2021-07-21 01:56:09 +0200

    feat: finished first template.py version

--- template.py
+++ template.py
@@ -77,9 +77,72 @@ class TemplateFile:
         return filename
 
 
-##Debugging
-test = TemplateFile("./input/test.txt", "./output")
-print(test.template())
-#print(test.templateEncrypt("tTb1gwfBaDCQ7n7lOWkA1Fv/TQ8FIAojb2w9K9A25V0="))
-#print(test.decrypt("tTb1gwfBaDCQ7n7lOWkA1Fv/TQ8FIAojb2w9K9A25V0="))
-test.write()
+helpText = "Usage without encryption: ./template.py -i <InputFolder> -o <OutputFolder>\n" \
+           "                          ./template.py --inputdir <InputFolder> --outputdir <OutputFolder>\n" \
+           "Usage with encryption: ./template.py --encrypt <EncryptionKey> --inputdir <InputFolder> --outputdir <OutputFolder>\n" \
+           "                       ./template.py -e <EncryptionKey> -i <InputFolder> -o <OutputFolder>\n" \
+           "                       ./template.py --decrypt <EncryptionKey> --inputdir <InputFolder> --outputdir <OutputFolder>\n" \
+           "                       ./template.py -d <EncryptionKey> -i <InputFolder> -o <OutputFolder>\n"
+
+# Get full command-line arguments
+full_cmd_arguments = sys.argv
+# Keep all but the first
+argument_list = full_cmd_arguments[1:]
+short_options = "he:d:i::o::"
+long_options = ["help", "encrypt", "decrypt", "inputdir", "outputdir"]
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+except getopt.error as err:
+    # print error, and return with an error code
+    print(str(err))
+    sys.exit(2)
+for current_argument, current_value in arguments:
+    if current_argument in ("-e", "--encrypt"):
+        encryptionMode = "encrypt"
+        encryptionKey = current_value
+    if current_argument in ("-d", "--decrypt"):
+        encryptionMode = "decrypt"
+        encryptionKey = current_value
+    if current_argument in ("-i", "--inputdir"):
+        encryptionMode = "none"
+        inputDir = current_value
+    if current_argument in ("-o", "--outputdir"):
+        encryptionMode = "none"
+        outputDir = current_value
+    if current_argument in ("-h", "--help"):
+        print(helpText)
+        sys.exit(2)
+
+# check if inputDir and outputDir are set
+if not inputDir or not outputDir:
+    print(helpText)
+    sys.exit(2)
+# create empty files list
+files = []
+# check if input is file or directory and create list with input files
+if os.path.isfile(inputDir):
+    files.append(inputDir)
+elif os.path.isdir(inputDir):
+    for file in os.listdir(inputDir):
+        files.append(os.path.join(inputDir, file))
+
+for file in files:
+    # create tempFile object from class TemplateFile
+    tempFile = TemplateFile(file, outputDir)
+    # use tempFile object functions
+    if encryptionMode == "encrypt":
+        tempFile.templateEncrypt(encryptionKey)
+        tempFile.write()
+    elif encryptionMode == "decrypt":
+        tempFile.decrypt(encryptionKey)
+        tempFile.write()
+    elif encryptionMode == "none":
+        tempFile.template()
+        tempFile.write()
+    else:
+        print("error")
+        sys.exit(4)
+    # delete object tempFile
+    del(tempFile)
+    # print empty line after each files
+    print("")
